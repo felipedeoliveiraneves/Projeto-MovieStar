acea5a9a29f99bdc6822e1e9ca77fcc96d3aa781
Felipe de oliveira <106187032+felipedeoliveiraneves@users.noreply.github.com>
2023-02-11 12:20:43 -0300

    aula 380

--- movie.php
+++ movie.php
@@ -27,6 +27,11 @@ if(empty($id)){
     }
 }
 
+//checar se o filme tem image 
+if($movie->image == "") {
+    $movie->image = "movie_cover.jpg";
+}
+
 //Checar se o filme é do usuário 
 
 $useOwnsMovie = false;
@@ -42,3 +47,79 @@ if(!empty($userData)) {
 
 ?>
 
+<div id="main-container" class="container-fluid">
+    <div class="row">
+        <div class="offset-md-1 col-md-6 movie-container">
+            <h1 class="page-title"><?= $movie->title ?></h1>
+            <p class="movie-details">
+                <span>Duração: <?= $movie->length ?></span>
+                <span class="pipe"></span>
+                <span><?= $movie->category ?></span>
+                <span class="pipe"></span>
+                <span><i class="fas fa-star"></i> 9 </span>
+            </p>
+            <iframe src="<?= $movie->trailer ?>" width="560" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encryted-media; gyroscope;
+            picture-in-picture" allowfullscreen></iframe>
+            <p><?= $movie->description ?></p>
+
+            </div>
+        </div>
+        <div class="col-md-4">
+           <div class="movie-image-container" style="background-image: url('<?= $BASE_URL ?>/img/movies/<?= $movie->image ?>')"></div>
+        </div>
+        <div class="offset-md-1 col-md-10" id="reviews-container">
+            <h3 id="reviews-title">Avaliações:</h3>
+            <!-- Verifica se habilita a review para usuario ou não -->
+            <div class="col-md-12" id="review-form-container">
+                <h4>Envie sua avaliação:</h4>
+                <p class="page-description">Preencha o formulario com a nota e comentario sobre o filme</p>
+                <form action="<?= $BASE_URL ?>review_process.php" id="review-form" method="POST">
+                    <input type="hidden" name="type" value="create">
+                    <input type="hidden" name="movies_id" value="<?= $movie->id ?>">
+                    <div class="form-group">
+                        <label for="rating">Nota do filme:</label>
+                        <select name="rating" id="rating" class="form-control">
+                            <option value="">Selecione</option>
+                            <option value="10">10</option>
+                            <option value="9">9</option>
+                            <option value="8">8</option>
+                            <option value="7">7</option>
+                            <option value="6">6</option>
+                            <option value="5">5</option>
+                            <option value="4">4</option>
+                            <option value="3">3</option>
+                            <option value="2">2</option>
+                            <option value="1">1</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="review">Seu comentário:</label>
+                        <textarea name="review" id="review" rows="3" class="form-control" placeholder="O que você achou do filme?"></textarea>
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Enviar comentario">
+                </form>
+            </div>
+            <!-- comentarios -->
+            <div class="col-md-12 review">
+                <div class="row">
+                   <div class="col-md-1">
+                    <div class="profile-image-container review-image" style="background-image: url('<?= $BASE_URL ?>img/users/user.png')"></div>
+                   </div>
+                    <div class="col-md-9 author-details-container">
+                        <h4 class="author-name">
+                            <a href="#">Felipe teste</a>
+                        </h4>
+                        <p><i class="fas fa-star"></i> 9</p>
+                    </div>
+                    <div class="col-md-12">
+                        <p class="comment-title">Comentario:</p>
+                        <p>Este é o comentario do usuario</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php 
+    require_once("templates/footer.php");
+?>
