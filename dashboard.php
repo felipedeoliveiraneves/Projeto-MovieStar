fbdb88fc0bf92a8af2844ff13c7af947498c02f5
Felipe de oliveira <106187032+felipedeoliveiraneves@users.noreply.github.com>
2023-02-13 21:59:10 -0300

    aula final

--- dashboard.php
+++ dashboard.php
@@ -25,27 +25,27 @@
     <div class="col-md-12" id="movies-dashboard">
     <table class="table">
         <thead>
-            <th scope="col">#</th>
-            <th scope="col">Titulo</th>
-            <th scope="col">Nota</th>
-            <th scope="col" class="actions-column">Ações</th>
+          <th scope="col">#</th>
+          <th scope="col">Título</th>
+          <th scope="col">Nota</th>
+          <th scope="col" class="actions-column">Ações</th>
         </thead>
         <tbody>
             <?php foreach($userMovies as $movie): ?>
             <tr>
-                <td scope="row"><?= $movie->id ?></td>
-                <td><a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="table-movie-title"><?= $movie->title ?></a></td>
-                <td><i class="fas fa-star"></i><?= $movie->rating ?></td>
-                <td class="actions-column">
-                  <a href="<?= $BASE_URL ?>editmovie.php?id=<?= $movie->id ?>" class="edit-btn">
-                    <i class="far fa-edit"></i> Editar
-                  </a>
-                  <form action="<?= $BASE_URL ?>movie_process.php" method="POST">
-                    <input type="hidden" name="type" value="delete">
-                    <input type="hidden" name="id" value="<?= $movie->id ?>">
-                    <button type="submit" class="delete-btn">
-                        <i class="fas fa-times"></i> Deletar
-                    </button>
+            <td scope="row"><?= $movie->id ?></td>
+            <td><a href="<?= $BASE_URL ?>movie.php?id=<?= $movie->id ?>" class="table-movie-title"><?= $movie->title ?></a></td>
+            <td><i class="fas fa-star"></i> <?= $movie->rating ?></td>
+            <td class="actions-column">
+              <a href="<?= $BASE_URL ?>editmovie.php?id=<?= $movie->id ?>" class="edit-btn">
+                <i class="far fa-edit"></i> Editar
+              </a>
+              <form action="<?= $BASE_URL ?>movie_process.php" method="POST">
+                <input type="hidden" name="type" value="delete">
+                <input type="hidden" name="id" value="<?= $movie->id ?>">
+                <button type="submit" class="delete-btn">
+                  <i class="fas fa-times"></i> Deletar
+                </button>
                   </form>  
                 </td>
             </tr>
--- templates/header.php
+++ templates/header.php
@@ -22,7 +22,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
     <link rel="short icon" href="<?= $BASE_URL ?>img/moviestar.ico"/>
@@ -40,11 +39,11 @@
                 <img src="<?= $BASE_URL ?>img/logo.svg" alt="MovieStar" id="logo">
                 <span id="moviestar-title">MovieStar</span>          
            </a> 
-            <button class="navbar-toggler" type="button" data_toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="search">  
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                 <i class="fas fa-bars"></i>
+             </button>
+            <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+                <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">  
                 <button class="btn my-2 my-sm-0" type="submit">
                     <i class="fas fa-search"></i>
                 </button>
@@ -79,7 +78,7 @@
     </header>
 <?php if(!empty($flassmessage["msg"])): ?>
 <div class="msg-container">
-    <p class="msg <?=$flassmessage["type"]?>"><?=$flassmessage["msg"] ?></p>
+    <p class="msg <?=$flassMessage["type"]?>"><?=$flassMessage["msg"] ?></p>
 </div>
 <?php endif; ?>
 
